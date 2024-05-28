992c4a40b0fded3074cbc3290fcdee358acbf3a6
vtisa <isai.valqui@tecsup.edu.pe>
2024-05-27 21:54:59 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Typography, Box, CircularProgress, Container } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
-function App() {
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  fontWeight: 'bold',
+  backgroundColor: 'black',
+  color: theme.palette.primary.contrastText,
+}));
+
+const App = () => {
+  const [productos, setProductos] = useState([]);
+  const [recuperado, setRecuperado] = useState(false);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/producto/')
+      .then((response) => response.json())
+      .then((prod) => {
+        setProductos(prod);
+        setRecuperado(true);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setRecuperado(true);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <Box my={4} display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h4" component="h1" gutterBottom color='red' >
+          Lista de Productos
+        </Typography>
+        {recuperado ? (
+          <TableContainer component={Paper} elevation={6}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Código</StyledTableCell>
+                  <StyledTableCell>Descripción</StyledTableCell>
+                  <StyledTableCell>Precio</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {productos.map((prod) => (
+                  <TableRow key={prod.codigo}>
+                    <TableCell>{prod.codigo}</TableCell>
+                    <TableCell>{prod.descripcion}</TableCell>
+                    <TableCell>s/.{typeof prod.precio === 'number' ? prod.precio.toFixed(2) : prod.precio}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <Box display="flex" justifyContent="center" alignItems="center" minHeight="50vh">
+            <CircularProgress size={60} />
+          </Box>
+        )}
+      </Box>
+    </Container>
   );
-}
+};
 
-export default App;
+export default App
